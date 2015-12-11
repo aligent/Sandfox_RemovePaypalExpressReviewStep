dcb49ff340f0c4d66528b5066e2f52cc09f9f5ae
Kevin Chaplin <kevin@aligent.com.au>
2015-12-11 15:27:50 +1030

    rewrite to returnFromPaypal add set shipping method in quote

---
+++ app/code/community/Sandfox/RemovePaypalExpressReviewStep/Model/Express/Checkout.php
@@ -0,0 +1,33 @@
+<?php
+
+class Sandfox_RemovePaypalExpressReviewStep_Model_Express_Checkout extends Mage_Paypal_Model_Express_Checkout
+{
+	/**
+	 * Run parent function then validate and add shipping method and save quote
+	 *
+	 *
+	 * @param string $token
+	 */
+	public function returnFromPaypal($token)
+	{
+		$quote = $this->_quote;
+		if (!$quote->getIsVirtual()) {
+			$shippingAddress = $quote->getShippingAddress();
+			if ($shippingAddress) {
+				// import shipping method
+				$code = '';
+				if ($this->_api->getShippingRateCode()) {
+					if ($code = $this->_matchShippingMethodCode($shippingAddress, $this->_api->getShippingRateCode())) {
+						// possible bug of double collecting rates :-/
+						$shippingAddress->setShippingMethod($code)->setCollectShippingRates(true);
+					}
+				}
+				$quote->getPayment()->setAdditionalInformation(
+					self::PAYMENT_INFO_TRANSPORT_SHIPPING_METHOD,
+					$code
+				);
+			}
+		}
+		$quote->collectTotals()->save();
+	}
+}
