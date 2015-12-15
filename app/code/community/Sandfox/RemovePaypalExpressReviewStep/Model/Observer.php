707ebae99f6b55cc46ec82850d367d7fafa84eff
Kevin Chaplin <kevin@aligent.com.au>
2015-12-15 14:03:27 +1030

    Redirect international orders to full checkout with message

--- app/code/community/Sandfox/RemovePaypalExpressReviewStep/Model/Observer.php
+++ app/code/community/Sandfox/RemovePaypalExpressReviewStep/Model/Observer.php
@@ -2,9 +2,27 @@
 
 class Sandfox_RemovePaypalExpressReviewStep_Model_Observer
 {
+	public function controllerActionPredispatchCheckoutOnepageIndex(Varien_Event_Observer $observer)
+	{
+		if ($checkoutNotificationMessage = Mage::getSingleton('core/session')->getCheckoutNotificationMessage()) {
+			Mage::getSingleton('checkout/session')->addNotice($checkoutNotificationMessage);
+		}
+	}
+
 	public function controllerActionPredispatchPaypalExpressReview(Varien_Event_Observer $observer)
 	{
-		Mage::app()->getResponse()->setRedirect(Mage::getUrl('*/*/placeOrder'));
+
+		$quote = Mage::getModel('checkout/cart')->getQuote();
+		$shippingAddress = $quote->getShippingAddress();
+		$payment = $quote->getPayment();
+
+		if ($shippingAddress->getCountryId() === 'AU' || $payment->getAdditionalInformation('paypal_express_checkout_shipping_overriden')) {
+			Mage::app()->getResponse()->setRedirect(Mage::getUrl('*/*/placeOrder'));
+		} else {
+			Mage::getSingleton('core/session')->setCheckoutNotificationMessage('Please proceed through full checkout for international orders');
+			Mage::app()->getResponse()->setRedirect(Mage::getUrl('checkout/onepage'));
+		}
+
 	}
 
 	public function controllerActionPredispatchPaypalExpressPlaceOrder(Varien_Event_Observer $observer)
